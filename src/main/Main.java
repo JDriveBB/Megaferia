ecf613a000ab1b1d31e4d2b6f8fe1e0ae1030c22
juan david benavides barros <juanbenavides@uninorte.edu.co>
2024-09-18 13:29:22 -0500

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+/*@author Juan Drive  desde de de dedede de dedeman*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello moto");
+    }
+}
